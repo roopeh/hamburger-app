5f84a93f6ff517434a3bad4b66da579013a43d87
roopeh <44685964+roopeh@users.noreply.github.com>
2021-07-25 19:42:01 +0000

    Create base for menu/toolbar thing

--- client/app/src/main/java/com/example/hamburger/MainActivity.java
+++ client/app/src/main/java/com/example/hamburger/MainActivity.java
@@ -1,13 +1,119 @@
 package com.example.hamburger;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Menu related
+    private String[] mToolbarItemTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    Toolbar toolbar;
+    private CharSequence mTitle;
+    androidx.appcompat.app.ActionBarDrawerToggle mDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialise menu
+        mTitle = getTitle();
+        mToolbarItemTitles = getResources().getStringArray(R.array.menu_items_array);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        mDrawerList = (ListView)findViewById(R.id.left_list);
+        initMenu();
+
+        // Create menu content
+        String[] menuItem = new String[3];
+        menuItem[0] = "Test";
+        menuItem[1] = "Test";
+        menuItem[2] = "Test";
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        MenuAdapter adapter = new MenuAdapter(this, R.layout.menu_item_row, menuItem);
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new MenuItemClickListener());
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer);
+        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        initMenuToggle();
+    }
+
+    private class MenuItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+    private void selectItem(int pos) {
+        Fragment fragment = null;
+        switch (pos) {
+            case 0:
+                // TODO
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.toolbar_content_frame, fragment).commit();
+
+            mDrawerList.setItemChecked(pos, true);
+            mDrawerList.setSelection(pos);
+            setTitle(mToolbarItemTitles[pos]);
+            mDrawerLayout.closeDrawer(mDrawerList);
+        } else {
+            Log.d("DEBUG_TAG", "Error while creating a fragment");
+        }
+    }
+
+    void initMenu() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+    }
+
+    void initMenuToggle() {
+        mDrawerToggle = new androidx.appcompat.app.ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NotNull MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        Objects.requireNonNull(getSupportActionBar()).setTitle(mTitle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
     }
 }
