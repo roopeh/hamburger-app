fa33a024d074f90a0102836b847e2e4433798884
roopeh <44685964+roopeh@users.noreply.github.com>
2021-08-02 19:02:56 +0000

    Replace ListView in menu with NavigationView

--- client/app/src/main/java/com/example/hamburger/MainActivity.java
+++ client/app/src/main/java/com/example/hamburger/MainActivity.java
@@ -3,24 +3,25 @@ package com.example.hamburger;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     // Menu related
-    private String[] mToolbarItemTitles;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     Toolbar toolbar;
@@ -34,71 +35,24 @@ public class MainActivity extends AppCompatActivity {
 
         // Initialise menu
         mTitle = getTitle();
-        mToolbarItemTitles = getResources().getStringArray(R.array.menu_items_array);
         mDrawerLayout = findViewById(R.id.drawer);
-        mDrawerList = findViewById(R.id.menuContent);
         ImageButton menuCloseButton = findViewById(R.id.menuCloseButton);
         initMenu();
 
-        // Create menu content
-        String[] menuItem = new String[mToolbarItemTitles.length];
-        System.arraycopy(mToolbarItemTitles, 0, menuItem, 0, mToolbarItemTitles.length);
+        NavigationView mDrawerList = findViewById(R.id.navigationView);
+        mDrawerList.setNavigationItemSelectedListener(this);
 
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(false);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        MenuAdapter adapter = new MenuAdapter(this, R.layout.menu_item_row, menuItem);
-        mDrawerList.setAdapter(adapter);
-        mDrawerList.setOnItemClickListener(new MenuItemClickListener());
-        mDrawerLayout = findViewById(R.id.drawer);
         mDrawerLayout.addDrawerListener(mDrawerToggle);
-        menuCloseButton.setOnClickListener(v -> mDrawerLayout.closeDrawers());
+        // TODO: fix this
+        //menuCloseButton.setOnClickListener(v -> mDrawerLayout.closeDrawers());
         initMenuToggle();
 
         // Load home fragment on startup
-        loadFragment(new HomeFragment(), 0);
-    }
-
-    private class MenuItemClickListener implements ListView.OnItemClickListener {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            selectItem(position);
-        }
-    }
-
-    private void selectItem(int pos) {
-        Fragment fragment = null;
-        switch (pos) {
-            // Home
-            case 0:
-                fragment = new HomeFragment();
-                break;
-            // Products
-            case 1:
-                fragment = new ProductsFragment();
-                break;
-            // Restaurants
-            case 2:
-                fragment = new RestaurantFragment();
-                break;
-            // Coupons
-            case 3:
-                fragment = new CouponFragment();
-                break;
-            // History
-            case 4:
-                fragment = new HistoryFragment();
-                break;
-            // Account
-            case 5:
-                // TODO
-                break;
-            default:
-                break;
-        }
-
-        loadFragment(fragment, pos);
+        loadFragment(new HomeFragment());
     }
 
     void initMenu() {
@@ -112,15 +66,12 @@ public class MainActivity extends AppCompatActivity {
         mDrawerToggle.syncState();
     }
 
-    void loadFragment(Fragment fragment, int pos) {
+    void loadFragment(Fragment fragment) {
         // TODO: handle fragment transition in background thread
         if (fragment != null) {
             FragmentManager fragmentManager = getSupportFragmentManager();
             fragmentManager.beginTransaction().replace(R.id.toolbar_content_frame, fragment).commit();
 
-            mDrawerList.setItemChecked(pos, true);
-            mDrawerList.setSelection(pos);
-            setTitle(mToolbarItemTitles[pos]);
             mDrawerLayout.closeDrawers();
         } else {
             Log.d("DEBUG_TAG", "Error while creating a fragment");
@@ -145,5 +96,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
         mDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+        switch (item.getItemId()) {
+            case R.id.menuHome:
+                fragment = new HomeFragment();
+                break;
+            case R.id.menuProducts:
+                fragment = new ProductsFragment();
+                break;
+            case R.id.menuRestaurants:
+                fragment = new RestaurantFragment();
+                break;
+            case R.id.menuCoupons:
+                fragment = new CouponFragment();
+                break;
+            case R.id.menuHistory:
+                fragment = new HistoryFragment();
+                break;
+            case R.id.menuAccount:
+                // TODO
+                break;
+            default:
+                break;
+        }
+
+        loadFragment(fragment);
+        return false;
     }
 }
