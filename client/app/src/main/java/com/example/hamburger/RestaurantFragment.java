3e0f59f6febe1d565d5e3f9fbccb6653975b6de9
roopeh <44685964+roopeh@users.noreply.github.com>
2021-08-13 14:56:12 +0000

    Create layout base for restaurant

--- client/app/src/main/java/com/example/hamburger/RestaurantFragment.java
+++ client/app/src/main/java/com/example/hamburger/RestaurantFragment.java
@@ -1,9 +1,17 @@
 package com.example.hamburger;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,6 +19,48 @@ public class RestaurantFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_restaurant, container, false);
+        ListView restaurantList = rootView.findViewById(R.id.restaurantList);
+
+        // Dummy content
+        List<String> tests = new ArrayList<>();
+        for (int i = 1; i < 20; ++i)
+            tests.add("Restaurant " + i +"\nOsoite, Kaupunki");
+
+        RestaurantListAdapter test = new RestaurantListAdapter(getContext(), tests);
+        restaurantList.setAdapter(test);
         return rootView;
+    }
+}
+
+class RestaurantListAdapter extends BaseAdapter {
+    Context _context;
+    List<String> _list;
+
+    public RestaurantListAdapter(Context cont, List<String> list) {
+        _context = cont;
+        _list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return _list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return _list.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = View.inflate(_context, R.layout.restaurant_list_item, null);
+        TextView content = view.findViewById(R.id.restaurantListItemText);
+        content.setText(_list.get(position));
+        return view;
     }
 }
